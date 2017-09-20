7c0632795e0d52e288970b6b8f8e49d83c6c8d29
Miguel Lopez <miguelcodeworld@gmail.com>
2017-09-20 00:33:23 -0400

    Update UseForInsteadOfImport.py

--- Static and Exception Handling/CreateModule/UseForInsteadOfImport.py
+++ Static and Exception Handling/CreateModule/UseForInsteadOfImport.py
@@ -1,8 +1,8 @@
 # The benefit of using for instead of import is
 # that you don't have to list the name of the module
-# whenever you use it
+# whenever you use it. Also you could add more def's by writing the varibles using commas. 
 
-from sum import getSum
+from sum import getSum       
 
 print("Sum:", getSum(1,2,3,4,5,34,12,158))
 
