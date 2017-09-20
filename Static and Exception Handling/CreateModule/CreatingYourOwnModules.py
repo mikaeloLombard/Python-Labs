ba262957532ac1c905d5045f1a39639091d70ee9
Miguel Lopez <miguelcodeworld@gmail.com>
2017-09-20 00:06:19 -0400

    Add files via upload
    Created as reference on how modules can be created and then called by using the import function, when necessary.

---
+++ Static and Exception Handling/CreateModule/CreatingYourOwnModules.py
@@ -0,0 +1,8 @@
+# I created the a python file named sum
+# It should be in the same folder as this .py file
+# A _pycache_  file was created automatically after this code was ran
+
+import sum
+
+print("Sum:", sum.getSum(1,2,3,4,5,34,123))
+
