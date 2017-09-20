2013e21e0f01a08455ed237da659c07659cd9bd6
Miguel Lopez <miguelcodeworld@gmail.com>
2017-09-19 23:38:42 -0400

    Add files via upload

---
+++ Static and Exception Handling/StaticVariables.py
@@ -0,0 +1,27 @@
+# Static field or static variable
+
+class Dog:
+
+    num_of_dogs = 0
+
+    def __init__(self, name="Unknown"):
+        self.name = name
+
+        Dog.num_of_dogs += 1
+
+    @staticmethod
+    def getNumOfDogs():
+        print("there are currently {} dogs".format(Dog.num_of_dogs))
+
+def main():
+
+    spot = Dog("Spot")
+
+    doug = Dog("Doug")
+
+    spot.getNumOfDogs()
+
+    doug.getNumOfDogs()
+
+main()
+
