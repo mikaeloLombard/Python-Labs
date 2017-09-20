96dea312ffe6318cb832c7587659c73e9261f81b
Miguel Lopez <miguelcodeworld@gmail.com>
2017-09-20 00:31:43 -0400

    Add files via upload
    The benefit of using for instead of import is that you don't have to list the name each time you want to use it. It will work like if the def from the module its present.

---
+++ Static and Exception Handling/CreateModule/UseForInsteadOfImport.py
@@ -0,0 +1,12 @@
+# The benefit of using for instead of import is
+# that you don't have to list the name of the module
+# whenever you use it
+
+from sum import getSum
+
+print("Sum:", getSum(1,2,3,4,5,34,12,158))
+
+
+
+
+
